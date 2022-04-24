2e23a93080b5542cf195cd5ea4f75ec9e1d5b8c2
Artur Pożyczka <artur.pozyczka@ppuc.pl>
2022-04-24 17:07:58 +0200

    minor fixes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<backfront>'));
+app.use(express.static(__dirname + '/dist/backfront'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/<backfront>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/backfront/index.html'));
 });
 app.listen(process.env.PORT || 8080);
